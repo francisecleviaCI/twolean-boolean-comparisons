1e5ab3a6a474ba435855437e573809016b5c943b
Francis Eclevia <franciseclevia@franciss-mbp.home>
2020-06-11 14:54:06 -0400

    homework

--- main.js
+++ main.js
@@ -2,8 +2,89 @@
  * YOUR CODE BELOW! *
  ********************/
 
+function moreThan5(number){
+  return number > 5;
+}
+let result = moreThan5 ();
+result;
 
+function topScore(score, topscore){
+  return score > topscore;
+}
+let result1 = topScore(90, 95);
+result1;
 
+function isInDanger(grade){
+  return grade > 60 && grade < 71;
+}
+let result2 = isInDanger(69);
+result2;
+
+function isCoasting(grade){
+  return grade > 72 && grade < 83;
+}
+let result3 = isCoasting();
+result3;
+
+function isSucceeding(grade){
+  return grade > 84 && grade < 92;
+}
+let result4 = isSucceeding(87);
+result4;
+
+function isFailing(grade){
+  return grade < 60;
+}
+let result5 = isFailing();
+result5;
+
+function isAcing(grade){
+  return grade > 92;
+}
+let results6 = isAcing(93);
+results6;
+
+function isStudent(str){
+  return str === 'student';
+}
+let results7 = isStudent('student');
+results7;
+
+function isTeacher(str){
+  return str === 'teacher';
+}
+let results8 = isTeacher('teacher');
+results8;
+
+function isAdmin(str){
+  return str === 'admin';
+}
+let results9 = isAdmin('teacher');
+results9;
+
+function isElementary(str){
+  return str === 'elementary';
+}
+let results10 = isElementary('elementary');
+results10;
+//*** */
+function areDifferentPeople(name1, name2){
+  return name1 !== name2;
+}
+let results11 = areDifferentPeople('Jordan, Kobe');
+results11; 
+
+function isMiddleSchoolTeacher(role, num){
+  return role === 'teacher' && num >= 6 && num <=8;
+}
+let results12 = isMiddleSchoolTeacher('teacher', 8);
+results12;
+
+function notAnElementarySchoolAdministrator(level, role){
+  return level !== 'elementary' && role !== 'administrator';
+}
+let results13 = notAnElementarySchoolAdministrator('middle', 'teacher');
+results13;
 
 
 /********************************************************************************************
